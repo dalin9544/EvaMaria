7dfcb2f754b239d67503a5af4fdea387e3d6d8fb
dalin9544 <108016806+dalin9544@users.noreply.github.com>
2022-06-23 13:46:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -406,8 +406,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ], [
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/TeamEvamaria')
+            InlineKeyboardButton('🔰 𝙾𝚆𝙽𝙴𝚁', url='https://t.me/NJaNu_yir')
+            InlineKeyboardButton('🤖 Updates', url='https://t.me/Hd_moviesmalyalam')
         ], [
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
             InlineKeyboardButton('😊 About', callback_data='about')
