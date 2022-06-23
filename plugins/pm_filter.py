d3a46eeedf9734c2ba10a84302c5d8a044259589
dalin9544 <108016806+dalin9544@users.noreply.github.com>
2022-06-23 14:12:16 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -406,11 +406,11 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
             InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
         ], [
-            InlineKeyboardButton('🔰 𝙾𝚆𝙽𝙴𝚁', url='https://t.me/NJaNu_yir'),
-            InlineKeyboardButton('🤖 Updates', url='https://t.me/Hd_moviesmalyalam')
+            InlineKeyboardButton('⚡️ 𝙾𝚆𝙽𝙴𝚁 ⚡️', url='https://t.me/NJaNu_yir'),
+            InlineKeyboardButton('🔔 Updates 🔔', url='https://t.me/Hd_moviesmalyalam')
         ], [
-            InlineKeyboardButton('ℹ️ Help', callback_data='help'),
-            InlineKeyboardButton('😊 About', callback_data='about')
+            InlineKeyboardButton('💬 Help 💬', callback_data='help'),
+            InlineKeyboardButton('ℹ️ About ℹ️', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
